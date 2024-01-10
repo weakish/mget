d9f2583ba7b8ada4e48a2bddce35f1dd7f69034c
weakish <weakish@gmail.com>
2024-01-10 15:27:19 +0000

    :new: shell wrapper of yt-dlp & wget

---
+++ README.md
@@ -0,0 +1,17 @@
+Download music from YouTube and other websites.
+
+It uses yt-dlp to download music from YouTube and wget from other websites (audio files linked in web pages).
+
+See source code for details.
+
+## Install
+
+Rename `mget.sh` to `mget` and put it in your `$PATH`.
+
+TODO add Makefile
+
+## Example
+
+```sh
+mget "https://leino-online.com/music/"
+```
---
+++ mget.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+case "$1" in
+    https://*.youtube.com/*)
+        # 251 is the audio only webm format using the opus codec at ~160kbps
+        yt-dlp --format 251 --extract-audio "$1"
+        ;;
+    *)
+        # TODO add more common audio formats
+        # TODO support upper case extensions, e.g. *.WAV
+        common_audio_format='.wav,.mp3,.ogg,.opus,.flac,.aac'
+        wget --accept="$common_audio_format" --execute='robots=off' --continue --recursive --level=2 --no-directories "$1"
+        ;;
+esac
