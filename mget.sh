82bf954f995632828988a77c8c5285f5d1cc82c5
weakish <weakish@gmail.com>
2024-02-05 15:09:41 +0000

    :new: instagram

--- mget.sh
+++ mget.sh
@@ -5,6 +5,10 @@ case "$1" in
         # 251 is the audio only webm format using the opus codec at ~160kbps
         yt-dlp --format 251 --extract-audio "$1"
         ;;
+    https://www.instagram.com/*)
+        ins_profile=$(echo "$1" | cut -d'/' -f4)
+        instaloader --quiet --latest-stamps latest-stamps.ini --comments --igtv "$ins_profile"
+        ;;
     *)
         # TODO add more common audio formats
         # TODO support upper case extensions, e.g. *.WAV
